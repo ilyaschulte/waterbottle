a5e610c7326d2a15d0c5fe7143b2056178fac9cc
“ilyaschulte” <ilyaschulte@gmail.com>
2024-05-02 14:15:37 +0200

    First

---
+++ index.js
@@ -0,0 +1,25 @@
+function fileUpload() {
+    const uploadedFiles = document.querySelector("#files")
+    const fileDisplay = document.querySelector("#display")
+
+    fileDisplay.innerHTML = '';
+
+
+}
+
+
+// document.querySelector("#opt1").setA
+
+// let clicks = 0;
+
+// function onclick() {
+//    document.querySelector("#opt1").setAttribute("style", "background-color: blue;");
+//    clicks = clicks + 1
+//    console.log(clicks % 3)
+
+    // if (clicks % 3 == 2) {  
+        //alert()
+    //}
+//}
+
+document.querySelector("#opt1").addEventListener("click", onclick)
